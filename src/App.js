dff02e73b6e1decf98a7dbc422dc64996b93d5e0
anderlfrias <anderl.friaas@gmail.com>
2022-04-13 10:58:51 -0400

    Creacion de los componentes necesarios para la app. NOTA: Aun Falta personalizacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {CssBaseline, Grid} from '@mui/material/';
+
+import Header from './components/Header/index';
+import List from './components/List/index';
+import Map from './components/Map/index';
+// import PlaceDetails from './components/PlaceDetails/index';
+
 
 const App = () => {
     return (
-        <div>
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
             <h1>Hello, World!</h1>
-        </div>
+        </>
     );
 }
 export default App;
